7454134d48e6f5ec5d5cabcf2b2db4d49fd85aa7
TamishaRutledge <tamisha.rutledge@gmail.com>
2020-04-12 14:50:44 -0500

    test

---
+++ hello.py
@@ -0,0 +1,19 @@
+"""
+firstString = "Hello World!"
+print(firstString)
+
+carName = "Lexas"
+print(carName)
+"""
+
+
+fruits = ["apple", "orange", "banana"]
+if "kiwi" in fruits:
+    print("She here!")
+else:
+    print("Nope!")
+
+    
+thisset = {"dog", "cat", "fish"}
+thisset.update(["snake", "turtle", "bird", "rabbit"])
+print(thisset)
