ebcc12781d75387247fd9d9c28852edef0a3ba9e
TamishaRutledge <tamisha.rutledge@gmail.com>
2020-04-12 15:32:28 -0500

    Variables are label

--- variablesAndDataTypes.py
+++ variablesAndDataTypes.py
@@ -1,22 +1,21 @@
-"""
+#This was from W3 schools 
 firstString = "Hello World!"
 print(firstString)
 
 carName = "Lexas"
 print(carName)
-"""
 
-"""
 fruits = ["apple", "orange", "banana"]
 if "kiwi" in fruits:
     print("She here!")
 else:
     print("Nope!")
-
-    
+ 
 thisset = {"dog", "cat", "fish"}
 thisset.update(["snake", "turtle", "bird", "rabbit"])
 print(thisset)
-"""
+
+#This is me starting Python Crash Course
+
 message = "Hello Python Crash Course reader!"
 print(message)
